e5d5b5b4af181831e77417a53a70dc097fc4a6fa
Michal901 <momot1122@wp.pl>
2024-01-26 10:13:41 +0100

    .

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const addItems = (e) => {
+  const galleryHTML = galleryItems
+    .map(
+      (image) =>
+        `<div class="gallery__item"><a class="gallery__link" href="${image.original}"><img class="gallery__image" src="${image.preview}" data-source = "${image.original}"  alt="${image.description}"></a></div> `
+    )
+    .join("");
+  gallery.insertAdjacentHTML("beforeend", galleryHTML);
+};
+
+addItems();
+
+gallery.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const lightbox = basicLightbox.create(`
+      <img src="${e.target.dataset.source}" width="800" height="600">
+    `);
+
+  lightbox.show();
+});
+
+console.log(gallery);
